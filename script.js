54f7154ce49dc932211dfa24fa04ec6300b8df6c
IGpsych-halle <institutsgruppe@psych.uni-halle.de>
2025-09-30 14:55:02 +0200

    updated script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // Optional future interactivity
   console.log("Seite geladen.");
 });
+
+ const acc = document.querySelectorAll(".accordion");
+
+    acc.forEach(button => {
+      button.addEventListener("click", () => {
+        const panel = button.nextElementSibling;
+        panel.classList.toggle("show");
+      });
+    });
